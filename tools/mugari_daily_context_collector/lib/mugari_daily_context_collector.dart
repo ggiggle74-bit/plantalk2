d9d64f9089062d1d16157cdda582d27e4e10a779
ggiggle74-bit <ggiggle74@gmail.com>
2026-07-18 15:24:01 +0900

    Export CR-2B query planning APIs

--- tools/mugari_daily_context_collector/lib/mugari_daily_context_collector.dart
+++ tools/mugari_daily_context_collector/lib/mugari_daily_context_collector.dart
@@ -1,3 +1,6 @@
 export 'src/models/daily_keyword_candidate.dart';
 export 'src/models/daily_keyword_context_document.dart';
+export 'src/models/search_document.dart';
+export 'src/planning/daily_query_planner.dart';
+export 'src/planning/daily_search_query.dart';
 export 'src/validation/daily_keyword_contract_validator.dart';
