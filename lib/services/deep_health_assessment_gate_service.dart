6ede9d1cf67e3c0b4419991a08e84745b5768880
ggiggle74-bit <ggiggle74@gmail.com>
2026-07-30 14:35:24 +0900

    Add deep health reservation lifecycle

--- lib/services/deep_health_assessment_gate_service.dart
+++ lib/services/deep_health_assessment_gate_service.dart
@@ -1,15 +1,31 @@
 import '../monetization/models/entitlement_check_context.dart';
 import '../monetization/models/entitlement_check_result.dart';
 
+class DeepHealthAssessmentAuthorization {
+  const DeepHealthAssessmentAuthorization({
+    required this.access,
+    this.reservationId,
+  });
+
+  final EntitlementCheckResult access;
+  final String? reservationId;
+}
+
 typedef AuthorizeAndReserveDeepHealthAssessmentCallback =
-    Future<EntitlementCheckResult> Function({
+    Future<DeepHealthAssessmentAuthorization> Function({
       required EntitlementCheckContext context,
     });
+typedef UpdateDeepHealthAssessmentReservationCallback =
+    Future<void> Function({required String reservationId});
 
 abstract class DeepHealthAssessmentGateService {
-  Future<EntitlementCheckResult> authorizeAndReserve({
+  Future<DeepHealthAssessmentAuthorization> authorizeAndReserve({
     EntitlementCheckContext context = const EntitlementCheckContext(),
   });
+
+  Future<void> commitReservation({required String reservationId});
+
+  Future<void> releaseReservation({required String reservationId});
 }
 
 class CallbackDeepHealthAssessmentGateService
@@ -17,14 +33,30 @@ class CallbackDeepHealthAssessmentGateService
   const CallbackDeepHealthAssessmentGateService({
     required AuthorizeAndReserveDeepHealthAssessmentCallback
     authorizeAndReserve,
-  }) : _authorizeAndReserve = authorizeAndReserve;
+    required UpdateDeepHealthAssessmentReservationCallback commitReservation,
+    required UpdateDeepHealthAssessmentReservationCallback releaseReservation,
+  }) : _authorizeAndReserve = authorizeAndReserve,
+       _commitReservation = commitReservation,
+       _releaseReservation = releaseReservation;
 
   final AuthorizeAndReserveDeepHealthAssessmentCallback _authorizeAndReserve;
+  final UpdateDeepHealthAssessmentReservationCallback _commitReservation;
+  final UpdateDeepHealthAssessmentReservationCallback _releaseReservation;
 
   @override
-  Future<EntitlementCheckResult> authorizeAndReserve({
+  Future<DeepHealthAssessmentAuthorization> authorizeAndReserve({
     EntitlementCheckContext context = const EntitlementCheckContext(),
   }) {
     return _authorizeAndReserve(context: context);
+  }
+
+  @override
+  Future<void> commitReservation({required String reservationId}) {
+    return _commitReservation(reservationId: reservationId);
+  }
+
+  @override
+  Future<void> releaseReservation({required String reservationId}) {
+    return _releaseReservation(reservationId: reservationId);
   }
 }
